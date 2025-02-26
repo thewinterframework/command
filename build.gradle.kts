b3c0c89aead3e02578029e9e9c7bf721e5282410
diegonighty <cardenasdiego003@gmail.com>
2025-02-26 17:52:05 -0600

    build: now on maven central!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,67 @@
 plugins {
     id("java")
     id("maven-publish")
+    id("io.deepmedia.tools.deployer") version "0.16.0"
     `java-library`
 }
 
 repositories {
-    mavenLocal()
-    maven("https://papermc.io/repo/repository/maven-public/")
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://repo.kryptonmc.org/releases")
     maven("https://repo.spongepowered.org/maven/")
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+deployer {
+    signing {
+        key.set(secret("winterSigningKey"))
+        password.set(secret("winterSigningPassphrase"))
+    }
+
+    // 1. Artifact definition.
+    // https://opensource.deepmedia.io/deployer/artifacts
+    content {
+        component {
+            fromJava() // shorthand for fromSoftwareComponent("java")
+        }
+    }
+
+    // 2. Project details.
+    // https://opensource.deepmedia.io/deployer/configuration
+    projectInfo {
+        description.set("The framework to make plugin creation easier than ever.")
+        url.set("https://github.com/thewinterframework/command")
+        license(MIT)
+
+        groupId.set(group.toString())
+        scm.fromGithub("thewinterframework", "command")
+        developer("Diego Cardenas", "diego.cardenas.v06@gmail.com")
+        artifactId.set(project.name)
+    }
+
+    // 3. Central Portal configuration.
+    // https://opensource.deepmedia.io/deployer/repos/central-portal
+    centralPortalSpec {
+        signing.key.set(secret("winterSigningKey"))
+        signing.password.set(secret("winterSigningPassphrase"))
+        auth.user.set(secret("winterRepositoryUsername"))
+        auth.password.set(secret("winterRepositoryPassword"))
+    }
+}
+
 dependencies {
     // Core
-    api("com.thewinterframework:core:1.0.0")
-    annotationProcessor("com.thewinterframework:core:1.0.0")
+    compileOnlyApi("com.thewinterframework:core:1.0.1")
+    annotationProcessor("com.thewinterframework:core:1.0.1")
 
     // Paper platform
-    compileOnlyApi("com.thewinterframework:paper:1.0.0")
+    compileOnlyApi("com.thewinterframework:paper:1.0.1")
 
     // External Command Framework
     api("org.incendo:cloud-paper:2.0.0-beta.10")
